d09c4856c6ba3028a913187a6fbe0ec8c20842e5
Nebojsa Vitomirov <nebojsa.vitomirov@hqplus.de>
2016-11-14 19:04:20 +0100

    +MailBoSelfHost application +Service reference on mailBo Consumer project

---
+++ MailBoServiceSelfHost/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace MailBoServiceSelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(MailBoService.SignOnService)))
+            {
+                host.Open();
+                Console.WriteLine("Service Started...");
+                Console.WriteLine("Press enter to stop the service.");
+                Console.ReadLine();
+                host.Close();
+            }
+        }
+    }
+}
