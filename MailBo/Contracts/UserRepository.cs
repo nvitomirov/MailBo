ee390fbe4385e69a1b125fc0ff6f7765a18160a7
Nebojsa Vitomirov <nebojsa.vitomirov@hqplus.de>
2016-11-15 17:20:43 +0100

    Refactoring userrepository

---
+++ MailBo/Contracts/UserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailBo.Contracts
+{
+    public interface UserRepository
+    {
+        bool Login(string username, string password);
+        string GetMessages(string username);
+    }
+}
---
+++ MailBo/Repositories/UserRepository.cs
@@ -0,0 +1,41 @@
+﻿using MailBo.Contracts;
+using System;
+using System.Text;
+
+namespace MailBo
+{
+    public class UserRepositoryImpl : UserRepository
+    {
+
+        public bool Login(string username, string password)
+        {
+            using (var client = new MailBoServiceRef.SignOnServiceClient())
+            {
+                return client.Login(username, password);
+            }
+        }
+        
+        public  string GetMessages(string username)
+        {
+            var sb = new StringBuilder();
+            using (var client = new MailBoServiceRef.SignOnServiceClient())
+            {
+                var messages = client.GetNews();
+                foreach (var message in messages)
+                {
+                    sb.Append("Absender: ")
+                        .Append(message.Sender)
+                        .Append(" Empfänger: ")
+                        .Append(message.Reciever)
+                          .Append(" Betreff: ")
+                        .Append(message.Subject)
+                        .Append(" Text: ")
+                        .Append(message.MessageBody)
+                        .Append(Environment.NewLine);
+                }
+                return sb.ToString();
+            }
+           
+        }
+    }
+}
