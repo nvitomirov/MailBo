eda98b6c9661c03a8f7862a7e829b098eadea7cf
Nebojsa Vitomirov <nebojsa.vitomirov@hqplus.de>
2016-11-17 22:21:33 +0100

    Unity framework, register repositories in container

---
+++ MailBo/Initialize.cs
@@ -0,0 +1,19 @@
+﻿using MailBo.Contracts;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailBo
+{
+    public class Initialize
+    {
+        public void Register()
+        {
+            IUnityContainer container = new UnityContainer();
+            container.RegisterType<UserRepository, UserRepositoryImpl>();
+        }
+    }
+}
